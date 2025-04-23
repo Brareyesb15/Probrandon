49b0027e3b078ce6e75447b9db0f865908ba82be
Brandon Reyes <brareyesb15@gmail.com>
2025-04-22 20:17:40 -0500

    cccccc

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -141,7 +141,7 @@ export class UserController {
       }
       
       const isPasswordValid = await user.comparePassword(currentPassword);
-      
+      //sssss
       if (!isPasswordValid) {
         res.status(401).json({
           success: false,
