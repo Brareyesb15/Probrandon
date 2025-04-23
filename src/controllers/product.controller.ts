8bbd6e4d487288b5ca08163006085801faea49f0
Brandon Reyes <brareyesb15@gmail.com>
2025-04-22 20:13:36 -0500

    cccc

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -4,7 +4,7 @@ import { logger } from '../config/logger';
 
 export class ProductController {
   /**
-   * Obtiene todos los productos cambiossss
+   * Obtiene todos los productos cambiosss
    * @route GET /api/products
    */
   static async getAllProducts(req: Request, res: Response): Promise<void> {
